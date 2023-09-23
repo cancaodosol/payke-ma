ee7fa3e6241e84f0669576a5e6de9cde20a2652a
Hideyuki Matsui <atagohan@yahoo.co.jp>
2023-09-23 14:00:33 +0900

    つぶやき編集画面作成のコマンドで作成できる分

---
+++ app/Http/Controllers/Tweet/Update/PutController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Tweet\Update;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PutController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
---
+++ command.md
@@ -0,0 +1,7 @@
+# コマンドなどのメモ書き
+
+## つぶやき編集画面作成
+
+sail artisan make:controller Tweet/Update/IndexController --invokable
+sail artisan make:controller Tweet/Update/PutController --invokable
+sail artisan make:request Tweet/UpdateRequest
