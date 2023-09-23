26e19070bef4cb366ad32762aa2d14b72cb3f0aa
Hideyuki Matsui <atagohan@yahoo.co.jp>
2023-09-23 15:21:59 +0900

    サービスコンテナと、依存性の注入

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
 use App\Models\Tweet;
+use App\Services\TweetService;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
@@ -11,10 +12,10 @@ class IndexController extends Controller
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, TweetService $tweetService)
     {
         // $tweets = Tweet::all()->sortByDesc('created_at');
-        $tweets = Tweet::orderBy('created_at', 'DESC')->get();
+        $tweets = $tweetService->getTweets();
         return view('tweet.index', ['tweets' => $tweets]);
     }
 }
