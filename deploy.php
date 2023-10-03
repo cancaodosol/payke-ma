d4bfed38f4174701ac25519339ec556b1e57c280
Hideyuki Matsui <atagohan@yahoo.co.jp>
2023-10-03 12:28:35 +0900

    Payke資材のアップロード、unzipの処理を作成。マイグレーションの実行が残。

--- deploy.php
+++ deploy.php
@@ -25,22 +25,44 @@ task('my_task', function () {
     // writeln('What time is it? {{current_date}}');
 });
 
-set('release_or_current_path', function () {
-    return 'hiderin.xyz/public_html';
-});
+set('release_or_current_path', 'hiderin.xyz/public_html');
 
-set('payke_zip_name', function () {
-    return 'payke-ec-752d7ee2ff92.zip';
-});
+set('payke_zip_name', 'payke-ec-752d7ee2ff92');
+
+set('payke_app_name', 'payke04');
 
 set('payke_zip_file_path', function () {
-    return '/payke_zips/{{payke_zip_name}}';
+    return '/payke_resources/{{payke_zip_name}}.zip';
+});
+
+set('payke_install_file_path', function () {
+    return '/payke_resources/install.php';
+});
+
+set('payke_env_file_path', function () {
+    return '/payke_resources/.env.php';
+});
+
+set('release_app_root', function () {
+    return 'hiderin.xyz/public_html/{{payke_app_name}}';
 });
 
 task('upload', function () {
     upload(__DIR__ . '{{payke_zip_file_path}}', '{{release_or_current_path}}');
 });
-    
+
+task('unzip', function () {
+    run('ls {{release_or_current_path}} -al');
+    run('unzip -u {{release_or_current_path}}/{{payke_zip_name}}.zip -d {{release_or_current_path}}');
+    run('mv {{release_or_current_path}}/{{payke_zip_name}} {{release_or_current_path}}/{{payke_app_name}}');
+});
+
+task('upload_payke_config', function () {
+    upload(__DIR__ . '{{payke_env_file_path}}', '{{release_app_root}}/app/Config');
+    upload(__DIR__ . '{{payke_install_file_path}}', '{{release_app_root}}/app/Config');
+    // run('{{release_app_root}}/app/Console/cake-for-Xserver plugin load Migrations');
+    // run('{{release_app_root}}/app/Console/cake-for-Xserver migrations migration run all');
+});
 
 // // Hooks
 
---
+++ payke_resources/install.php
@@ -0,0 +1,17 @@
+<?php
+$config = array (
+  'Install' => 
+  array (
+    'installed' => true,
+    'redirect' => 
+    array (
+      'url' => 
+      array (
+        'plugin' => NULL,
+        'controller' => 'users',
+        'action' => 'login',
+        'admin' => true,
+      ),
+    ),
+  ),
+);
