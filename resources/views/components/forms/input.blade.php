fe1d945c2750d8d68b2c3097579a57767cf01291
Hideyuki Matsui <atagohan@yahoo.co.jp>
2023-10-25 00:02:34 +0900

    入力項目の説明文のデザイン調整。

--- resources/views/components/forms/input.blade.php
+++ resources/views/components/forms/input.blade.php
@@ -5,7 +5,7 @@
             <input type="text" name="{{ $name }}" id="{{ $name }}" autocomplete="{{ $name }}" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="{{ $example ?? ''}}">
         </div>
         @if($explain ?? false)
-            <p class="mt-3 text-sm leading-6 text-gray-600">{{ $explain }}</p>
+            <p class="mt-1 text-xs leading-6 text-gray-500">{{ $explain }}</p>
         @endif
     </div>
 </div>
