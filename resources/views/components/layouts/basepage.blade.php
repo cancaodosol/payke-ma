a697b2210351b575802ba0ae077be35fe550c47a
Hideyuki Matsui <atagohan@yahoo.co.jp>
2023-10-24 16:37:59 +0900

    使っていないメニューを一旦コメントアウト。

--- resources/views/components/layouts/basepage.blade.php
+++ resources/views/components/layouts/basepage.blade.php
@@ -82,15 +82,15 @@
                         </svg>
                         Reports
                         </a>
-                    </li>
+                    </li> -->
                     </ul>
                 </li>
-                <li>
+                <!-- <li>
                     <div class="text-xs font-semibold leading-6 text-gray-400">Your teams</div>
                     <ul role="list" class="-mx-2 mt-2 space-y-1">
-                    <li>
+                    <li> -->
                         <!-- Current: "bg-gray-50 text-indigo-600", Default: "text-gray-700 hover:text-indigo-600 hover:bg-gray-50" -->
-                        <a href="#" class="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold">
+                        <!-- <a href="#" class="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold">
                         <span class="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600">H</span>
                         <span class="truncate">Heroicons</span>
                         </a>
@@ -117,7 +117,7 @@
                     </svg>
                     Settings
                     </a>
-                </li>
+                </li> -->
                 </ul>
             </nav>
             </div>
