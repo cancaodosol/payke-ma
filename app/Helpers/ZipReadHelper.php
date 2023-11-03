07e23fcecd4679fa224e9d420a00be58628d6397
Hideyuki Matsui <atagohan@yahoo.co.jp>
2023-11-03 18:29:06 +0900

    PaykeZipのパスの指定の基準を、アプリルートに固定。

--- app/Helpers/ZipReadHelper.php
+++ app/Helpers/ZipReadHelper.php
@@ -6,6 +6,13 @@ use ZipArchive;
 
 class ZipReadHelper
 {
+
+    public string $root_dir;
+    public function __construct()
+    {
+        $this->root_dir = dirname(__FILE__)."/../../";
+    }
+
     // 参考記事：https://symfoware.blog.fc2.com/blog-entry-1891.html
     // 公式Doc：https://www.php.net/manual/ja/class.ziparchive.php
     public function get_payke_version($targetZipPath)
@@ -14,11 +21,12 @@ class ZipReadHelper
 
         $version = 'v-1.-1.-1';
         $zip = new ZipArchive();
-        if(!$zip->open($targetZipPath))
+        if(!$zip->open($this->root_dir.$targetZipPath))
         {
             return $version;
         }
 
+        // Paykeのバージョンを見るため、constants.php ファイルを探す。
         $filename = '';
         for($i = 0; $i < $zip->numFiles; $i++)
         {
