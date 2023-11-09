0822fd6e95dba906ff61e817aed1f7e71d589253
Hideyuki Matsui <atagohan@yahoo.co.jp>
2023-11-10 03:15:08 +0900

    サンプルコードの削除。

--- routes/web.php
+++ routes/web.php
@@ -32,12 +32,6 @@ require __DIR__.'/auth.php';
 
 Route::redirect('/', '/payke_user');
 
-Route::get('/sample', [\App\Http\Controllers\Sample\IndexController::class, 'show']);
-Route::get('/sample/{id}', [\App\Http\Controllers\Sample\IndexController::class, 'showId']);
-
-Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)
-    ->name('tweet.index');
-
 Route::get('/payke_host', [App\Http\Controllers\PaykeHostController::class ,'view_all'])
     ->name('payke_host.index');
 
@@ -101,13 +95,5 @@ Route::post('/search', \App\Http\Controllers\Search\IndexController::class)
 Route::middleware('auth')->group(
     function()
     {
-        Route::post('/tweet/create', \App\Http\Controllers\Tweet\CreateController::class)
-            ->name('tweet.create');
-        Route::get('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\IndexController::class)
-            ->name('tweet.update.index');
-        Route::put('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\PutController::class)
-            ->name('tweet.update.put');
-        Route::delete('/tweet/delete/{tweetId}', \App\Http\Controllers\Tweet\DeleteController::class)
-            ->name('tweet.delete');
     }
 );
