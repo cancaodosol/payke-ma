0c895b242a7132203c72f00a83260fc157487b18
Hideyuki Matsui <atagohan@yahoo.co.jp>
2023-11-14 11:49:19 +0900

    ユーザー編集画面、微調整。

--- resources/views/payke_user/edit.blade.php
+++ resources/views/payke_user/edit.blade.php
@@ -21,7 +21,8 @@
             <x-forms.list name="status" value="{{ $user->status }}" label="ステータス" :list="$statuses"/>
             <x-forms.list name="payke_host_db_id" value="{{ $user->host_db_id() }}" label="サーバー / DB" :list="$host_dbs" addPageLink="{{ route('payke_db.create') }}"/>
             <x-forms.list name="payke_resource_id" value="{{ $user->payke_resource_id }}" label="Paykeバージョン" :list="$resources" addPageLink="{{ route('payke_resource.index') }}"/>
-            <x-forms.input name="user_app_name" value="{{ $user->user_app_name }}" label="公開アプリ名" disabled="true" example="例) tarotaro7" explain="この名前が、サブディレクトリ名となります。"/>
+            <input type="hidden" name="user_app_name" value="{{ $user->user_app_name }}"/>
+            <x-forms.input name="__user_app_name" value="{{ $user->user_app_name }}" label="公開アプリ名" disabled="true" example="例) tarotaro7" explain="この名前が、サブディレクトリ名となります。"/>
             <x-forms.checkbox name="enable_affiliate" value="{{ $user->enable_affiliate }}" label="アフィリエイト機能" cbText="有効にする"/>
             <x-forms.input name="user_name" value="{{ $user->user_name }}" label="利用者名" explain="管理画面に表示する名前です。"/>
             <x-forms.input name="email_address" value="{{ $user->email_address }}" label="メールアドレス" example="xxxxx@xxxxx.com"/>
@@ -31,7 +32,7 @@
     </div>
     <!-- 保存ボタン -->
     <div class="mt-6 flex items-center justify-end gap-x-6">
-        <button type="submit" class="inline-flex justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">新規作成する</button>
+        <button type="submit" class="inline-flex justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">更新する</button>
     </div>
     </form>
 </x-layouts.basepage>
