aed9ecf5ec7c186b4eb22411a823b55a17794574
Hideyuki Matsui <atagohan@yahoo.co.jp>
2024-03-19 23:30:43 +0900

    各Paykeバージョンの利用者の表示数を無制限に。

--- resources/views/payke_resource/index.blade.php
+++ resources/views/payke_resource/index.blade.php
@@ -62,20 +62,17 @@
                             <a href="{{ route('payke_user.index.paykeId', ['paykeId' => $payke->id]) }}" class="text-xs text-indigo-600 hover:text-indigo-900">
                                 利用者 {{ $payke->PaykeUsers()->count() }}人
                             </a>
-
-                            @if($payke->PaykeUsers()->count() <= 10)
-                                (
-                                @foreach($payke->PaykeUsers as $user)
-                                <a href="{{ route('payke_user.profile', ['userId' => $user->id]) }}" class="text-indigo-600 hover:text-indigo-900">
-                                    @if($loop->last)
-                                    {{ $user->user_name }}
-                                    @else
-                                    {{ $user->user_name }},
-                                    @endif
-                                </a>
-                                @endforeach
-                                )
-                            @endif
+                            (
+                            @foreach($payke->PaykeUsers as $user)
+                            <a href="{{ route('payke_user.profile', ['userId' => $user->id]) }}" class="text-indigo-600 hover:text-indigo-900">
+                                @if($loop->last)
+                                {{ $user->user_name }}
+                                @else
+                                {{ $user->user_name }},
+                                @endif
+                            </a>
+                            @endforeach
+                            )
                         </div>
                         @endif
                         <br><br>
