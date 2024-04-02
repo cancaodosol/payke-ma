e852a5e960cbdf61b1fd233f2aa58a5dfda9666b
Hideyuki Matsui <atagohan@yahoo.co.jp>
2024-04-02 09:55:29 +0900

    初回の管理ユーザーメールアドレスをGMAILに。

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,7 @@ class UserFactory extends Factory
         return [
             'name' => "松井英之",
             'role' => User::ROLE__ADMIN,
-            'email' => "atagohan@yahoo.co.jp",
+            'email' => "canac0.d0.s0lh1de.m24w@gmail.com",
             'email_verified_at' => now(),
             'password' => '$2y$10$AtCZrDj1p4V0weeDX6TpCOrhjhr3cp2aKQUKzBkvIa6z/i7.uZFfa', // password
             'remember_token' => Str::random(10),
