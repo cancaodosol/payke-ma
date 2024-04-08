cdda80d47767f37b24c9bae5164eb476de7978db
Hideyuki Matsui <atagohan@yahoo.co.jp>
2024-04-08 23:13:26 +0900

    ファイル入力項目が若干はみ出てたのを修正。

--- resources/views/components/forms/file.blade.php
+++ resources/views/components/forms/file.blade.php
@@ -2,7 +2,7 @@
     <label for="{{ $name }}" class="block text-sm lg:text-right font-medium leading-6 text-gray-900 sm:pt-1.5">{{ $label }}</label>
     <div class="mt-2 sm:col-span-2 sm:mt-0">
         <div class="flex sm:max-w-md">
-            <input type="file" name="{{ $name }}" id="{{ $name }}" autocomplete="{{ $name }}" value="{{ old($name, '') }}" class="block w-full text-xs text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" placeholder="{{ $example ?? ''}}">
+            <input type="file" name="{{ $name }}" id="{{ $name }}" autocomplete="{{ $name }}" value="{{ old($name, '') }}" class="block w-full text-xs text-gray-900 border border-gray-300 rounded cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" placeholder="{{ $example ?? ''}}">
         </div>
         @if($explain ?? false)
             <p class="mt-1 text-xs leading-6 text-gray-500">{{ $explain }}</p>
