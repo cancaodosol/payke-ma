5f03a745e74d52af16384e303f9c14830b46ce8b
Hideyuki Matsui <atagohan@yahoo.co.jp>
2024-04-22 11:53:26 +0900

    購入後案内メールの文面のIDをメールアドレスに。

--- resources/views/email/payke_ec_ordered.blade.php
+++ resources/views/email/payke_ec_ordered.blade.php
@@ -4,8 +4,8 @@ Payke EC のご購入ありがとうございます。<br>
 <br>
 こちらから初期設定を行い、Payke ECをご利用ください。<br>
 ーーーーーーーーーーーーーーー<br>
-URL: <a href="{{$login_url}}">{{$login_url}}</a><br>
-ID: {{$login_id}}<br>
+URL: <a href="{{route('login')}}">{{route("login")}}</a><br>
+メールアドレス: {{$login_id}}<br>
 パスワード： {{$login_password}}<br>
 ーーーーーーーーーーーーーーー<br>
 <br>
